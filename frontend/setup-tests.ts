43881d8ec39c3896429dc4ec7a9efef577e6a3ca
Dustin <dees.dustin5@gmail.com>
2025-06-24 16:49:22 -0500

    fixed mediastream error

--- frontend/setup-tests.ts
+++ frontend/setup-tests.ts
@@ -1,6 +1,17 @@
 // setup-tests.ts
 import '@testing-library/jest-dom';
 
+// setup-tests.ts
+
+// @ts-ignore
+globalThis.MediaStream = class {
+  getTracks() {
+    return [];
+  }
+};
+
+
+
 Object.defineProperty(global.navigator, 'mediaDevices', {
   writable: true,
   value: {
